847f8c9377fd6d333029d7e3859ecf02db741d5e
Écio Ferraz <ecioferraz@gmail.com>
2023-07-12 11:52:36 -0300

    feat: Caroussel under development

--- src/templates/Caroussel/index.tsx
+++ src/templates/Caroussel/index.tsx
@@ -1,9 +1,10 @@
 /* eslint-disable react/jsx-key */
 'use client';
 
-import { Button } from '@mui/material';
 import AliceCarousel from 'react-alice-carousel';
 import 'react-alice-carousel/lib/alice-carousel.css';
+import './styles.css';
+import Image from 'next/image';
 
 const responsive = {
   0: {
@@ -16,26 +17,21 @@ const responsive = {
 };
 
 const items = [1, 2, 3, 4, 5].map((num) => (
-  <div
-    className="item"
-    style={{
-      backgroundImage: "url('/little-mermaid.jpg')",
-      backgroundPosition: 'center',
-      backgroundRepeat: 'no repeat',
-      backgroundSize: 'cover',
-      border: '1px solid black',
-      borderRadius: 20,
-      height: 475,
-      marginInline: 20,
-      padding: 20,
-    }}
-    data-value={num}
-  >
-    <h1>A Pequena Sereia</h1>
-    <span>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, sit iste
-      itaque sint enim iure!
-    </span>
+  <div className="item slide" data-value={num}>
+    <Image
+      src="/little-mermaid-caroussel.png"
+      alt=""
+      fill
+      objectFit="cover"
+      style={{ zIndex: -1 }}
+    />
+    <div className="content">
+      <h1>A Pequena Sereia</h1>
+      <span>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum, sit
+        iste itaque sint enim iure!
+      </span>
+    </div>
   </div>
 ));
 
@@ -45,7 +41,7 @@ export default function Carousel() {
       mouseTracking
       items={items}
       responsive={responsive}
-      autoPlay
+      // autoPlay
       autoPlayInterval={5000}
       infinite
       paddingLeft={55}
